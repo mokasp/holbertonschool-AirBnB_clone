34acbc613ad142936775e13c8e4cf9394f66a5ba
michaellgans <michaellgans@hotmail.com>
2023-10-06 10:39:04 -0700

    M - adds documentation to Task 6

--- console.py
+++ console.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
+""" Module containing the class HBNBCommand """
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
     """
     A class that runs the AirB&B Console
+    As the entry point to the command interpreter
 
     Attributes
     ~~~~~~~~~~
