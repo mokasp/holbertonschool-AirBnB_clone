7e1a78684a00ebde326273bda28d353aea6af15f
michaellgans <michaellgans@hotmail.com>
2023-10-07 12:14:30 -0700

    M - Created state, city, amenity, place, review

--- models/state.py
+++ models/state.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" Module containing the class State """
+# Michael 1:38 PM 10/7
+import BaseModel
+
+
+class State(BaseModel):
+    """
+    A class called State with a public attribute
+    for the name of the state.
+
+    Attributes
+    ~~~~~~~~~~
+        name (string):
+            Name of the state
+    """
+    name = ""
