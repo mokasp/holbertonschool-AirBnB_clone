787cb01c79e419bdfd978980f47cc54c50c011f8
michaellgans <michaellgans@hotmail.com>
2023-10-08 11:50:49 -0700

    M - updated console WIP

--- wip_console.py
+++ wip_console.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Module containing the class HBNBCommand """
-# Up to date version 2:32 PM
+# Michael Updated at 1:50 PM 10/8
 import cmd
 import models
 from models.base_model import BaseModel
@@ -15,106 +15,164 @@ class HBNBCommand(cmd.Cmd):
     ~~~~~~~~~~
     prompt (string):
         Displayed as the cursor for the user.
+    classes (dictionary):
+        All subclasses available for the user to call.
 
     Methods
     ~~~~~~~
     do_emptyline():
         Do nothing when an empty line is entered.
     do_quit():
-        Exits the program when called by the user
+        Exits the program when called by the user.
+        User Command - quit
     do_EOF():
         Enter the command "EOF" to quit the program
-        Handles "CTRL+D"
+        Handles CTRL+D
+        User Command - EOF
+    do_create():
+        Enter the command "create <class>" to make a new Object.
+        User Command - create <class>
+    do_show():
+        Enter the command "show <class> <id>" to display
+        that specific object.
+        User Command - show <class> <id>
+    do_destroy():
+        Enter the command "destroy <class> <id>" to remove
+        that specific object.
+        User Command - destroy <class> <id>
+    do_all():
+        Enter the command "all" to print everything in storage.
+        Enter the command "all <class>" to print anything created
+        with that class.
+        User Command - all OR all <class>
+    do_update():
+        Enter the command:
+        update <class> <id> <new attribute> "<new attribute value>"
+        to add a new attribute to an instance of the specific class.
+        User Command -
+        update <class> <id> <new attribute> "<new attribute value>"
     """
 
     prompt = "(hbnb) "
     classes = {"BaseModel": BaseModel}
 
     def emptyline(self):
-        """ Do nothing when an empty line is entered"""
+        """
+        Do nothing when an empty line is entered.
+        """
         pass
+    # Completed
 
     def do_quit(self, line):
-        """ Quit command to exit the program """
+        """
+        Enter the command "quit" to exit the program.
+        """
         return True
+    # Completed
 
     def do_EOF(self, line):
-        """
+        """ 
         Enter the command "EOF" to quit the program
         Handles "CTRL D"
         """
         return True
+    # Completed
 
     def do_create(self, line):
         """
-        Create command to make new Object
+        Enter the command "create <class>" to make a new Object.
         """
         if len(line) == 0:
-            print("** class name missing **")
+            print("** class name missing **") # Completed
             return
         args = line.split()
         object_type = args[0]
         try:
             new_object = self.classes[object_type]()
-            print(new_object.id)
+            print(new_object.id) # Completed
         except KeyError:
-            print("** class doesnt exist **")
+            print("** class doesn\'t exist **") # Completed
+    # Completed
 
     def do_show(self, line):
         """
-        Show command to display an object
+        Enter the command "show <class> <id>" to display
+        that specific object
         """
         if len(line) == 0:
-            print("** class name missing **")
+            print("** class name missing **") # Completed
             return
         args = line.split()
         if args[0] not in self.classes:
-            print("** class doesn't exist **")
+            print("** class doesn't exist **") # Completed
             return
         elif len(args) < 2:
-            print("** instance id missing **")
+            print("** instance id missing **") # Completed
             return
         key = f"{args[0]}.{args[1]}"
         all_of_them = models.storage.all()
         try:
-            print(all_of_them[key])
+            print(all_of_them[key]) # Completed
         except KeyError:
-            print("** no instance found **")
-        
+            print("** no instance found **") # Completed
+    # Completed
+
     def do_destroy(self, line):
-        """ Destroy command to remove an object from storage
+        """
+        Enter the command "destroy <class> <id>" to remove
+        that specific object
         """
         if len(line) == 0:
-            print("** class name missing **")
+            print("** class name missing **") # Completed
             return
         args = line.split()
         if args[0] not in self.classes:
-            print("** class doesn't exist **")
+            print("** class doesn\'t exist **") # Completed
             return
         elif len(args) < 2:
-            print("** instance id missing **")
+            print("** instance id missing **") # Completed
             return
         key = f"{args[0]}.{args[1]}"
         try:
-            del models.storage[key]
-            models.storage.save()
+            dictionary = models.storage.all()
+            dictionary.pop(key)
+            models.storage.save() # Completed
         except KeyError:
-            print("** no instance found **")
+            print("** no instance found **") # Completed
     
     def do_all(self, line):
         """
-        All command to display all objects, or all instances of a specific\
-        class, in storage """
+        Enter the command "all" to print everything in storage.
+        Enter the command "all <class>" to print anything created
+        with that class.
+        """
+        args = line.split()
+        dictionary = models.storage.all()
+
         if len(line) == 0:
-            for key in models.storage.all():
-                print(models.storage.all()[key])
+            for key in dictionary:
+                print(dictionary[key])
+        elif args[0] not in self.classes:
+            print("** class doesn\'t exist **") # Completed
+            return
         else:
-            args = line.split()
-            print("~~~ im stuck here ~~~")
+            for key in dictionary:
+                var = key.split(".")
+                if var[0] == args[0]:
+                    print(dictionary.get(key)) # Completed
+# Completed
+
+    def do_test(self, line):
+        """
+        TEST STUFF
+        """
+        print(models.storage.__class__.__name__)
 
     def do_update(self, line):
         """
-        Update command to add attributes to existing objects
+        Enter the command:
+        update <class> <id> <new attribute> "<new attribute value>"
+        to add a new attribute to an instance of the specific class.
         """
         print("update")
         if len(line) == 0:
@@ -122,7 +180,7 @@ class HBNBCommand(cmd.Cmd):
             return
         args = line.split()
         if args[0] not in self.classes:
-            print("** class doesn't exist **")
+            print("** class doesn\'t exist **")
             return
         elif len(args) == 1:
             print("** instance id missing **")
@@ -138,10 +196,10 @@ class HBNBCommand(cmd.Cmd):
             print("** attribute name missing **")
             return
         elif len(args) == 3:
-            print("** ~THIS ONE NEEDS FIXING~ value missing **")
+            print("** TO BE CODED **")
             return
         else:
-            print("~~~ im stuck here ~~")
+            print("~~~ TO BE CODED ~~")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
