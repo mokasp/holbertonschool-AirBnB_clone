19cc20c3e3e55b71bb94e82a5a33b35a685eb94b
mokasp <kasperbouldin@gmail.com>
2023-10-08 21:30:25 -0500

    first test for file storage!

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+""" File to test file_storage """
+import unittest
+import os
+import datetime
+
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+class TestFileStorage(unittest.TestCase):
+    """ Class to test BaseModel """
+
+    def setUp(self):
+        """ Setting up """
+        if os.path.exists('file.json'):
+            os.remove("file.json")
+        else:
+            pass
+
+    def tearDown(self):
+        """ Tearing Down """
+        pass
+
+    def test_privateAttributes(self):
+        storage = FileStorage()
+        with self.assertRaises(AttributeError):
+            print(storage.objects)
+
+
+if __name__ == 'main__':
+    unittest.main()
