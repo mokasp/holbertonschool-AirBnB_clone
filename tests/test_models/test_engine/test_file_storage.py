6bc7a78996e7da8c9a60afb1c918854b1448ac3e
mokasp <kasperbouldin@gmail.com>
2023-10-08 21:41:33 -0500

    rechecking same test and one additional test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -8,7 +8,7 @@ from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 class TestFileStorage(unittest.TestCase):
-    """ Class to test BaseModel """
+    """ Class to test FileStorage """
 
     def setUp(self):
         """ Setting up """
@@ -19,12 +19,27 @@ class TestFileStorage(unittest.TestCase):
 
     def tearDown(self):
         """ Tearing Down """
-        pass
+        if os.path.exists('file.json'):
+            os.remove("file.json")
+        else:
+            pass
 
-    def test_privateAttributes(self):
+    def test_privateObjects(self):
+        """ test to check if __objects is truly private """
         storage = FileStorage()
         with self.assertRaises(AttributeError):
-            print(storage.objects)
+            print(storage.__objects)
+
+    def test_privateFile(self):
+        """ tests to check if __file_path is truly private """
+        storage = FileStorage()
+        with self.assertRaises(AttributeError):
+            print(storage.__file_path)
+
+    def test_all(self):
+        """ tests to check if all method returns a dictionary """
+        storage = FileStorage()
+        self.assertIsInstance(storage.all(), dict)
 
 
 if __name__ == 'main__':
