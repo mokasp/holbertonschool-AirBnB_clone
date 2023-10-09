1ad1249e4fb932fe7d8fce793633620cc7f4fba9
michaellgans <michaellgans@hotmail.com>
2023-10-09 10:52:11 -0700

    M - Adds tests

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -11,32 +11,56 @@ class TestBaseModel(unittest.TestCase):
 
     def setUp(self):
         """ Setting up """
-        self.model = BaseModel()
-        self.model.save()
+        self.models = BaseModel()
+        self.models.save()
 
     def tearDown(self):
         """ Tearing Down """
-        del self.model
+        del self.models
         os.remove("file.json")
 
     def test_init(self):
         """  Tests id data type """
-        var = BaseModel()
-        self.assertIsInstance(var.id, str)
+        self.assertIsInstance(BaseModel().id, str)
 
     def test_init1(self):
         """  Tests created_at data type """
-        var = BaseModel()
-        self.assertIsInstance(var.created_at, datetime.datetime)
+        self.assertIsInstance(BaseModel().created_at, datetime.datetime)
 
     def test_init2(self):
         """  Tests updated_at data type """
-        var = BaseModel()
-        self.assertIsInstance(var.updated_at, datetime.datetime)
+        self.assertIsInstance(BaseModel().updated_at, datetime.datetime)
 
-    """self.assertIsNotNone - check for none
-    self.assertIsInstance - check for type
-    self.assertEqual - in the init (almost?)
+    def test_init3(self):
+        """  Tests id data type """
+        self.assertIsNotNone(BaseModel().id)
+
+    def test_init4(self):
+        """  Tests created_at data type """
+        self.assertIsNotNone(BaseModel().created_at)
+
+    def test_init5(self):
+        """  Tests updated_at data type """
+        self.assertIsNotNone(BaseModel().updated_at)
+    
+    def test_init6(self):
+        """ Tests updated_at vs created_at """
+        my_model = BaseModel()
+        first_save = my_model.created_at
+        my_model.save()
+        first_update = my_model.updated_at
+        self.assertNotEqual(first_save, first_update)
+
+    def test_save(self):
+        """ Tests created_at and updated_at are the same """
+        my_model = BaseModel()
+        first_update = my_model.updated_at
+        my_model.save()
+        self.assertNotEqual(my_model.updated_at, first_update)
+
+
+    """
+
 
     save
     self.assertNotEqual - save
